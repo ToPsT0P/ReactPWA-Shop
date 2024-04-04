39aaa5db8f2225dee7624c84100bba70312778cb
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-04-04 12:05:08 +0300

    start cart layout

---
+++ src/pages/MyCart/MyCart.jsx
@@ -0,0 +1,13 @@
+import styles from "./MyCart.module.css"
+import React from "react"
+
+const MyCart = () =>
+{
+    return(
+        <div>
+            <h1>MY CART</h1>
+        </div>
+    )
+}
+
+export default MyCart
