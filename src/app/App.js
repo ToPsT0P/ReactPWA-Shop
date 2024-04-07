4922996d56a4039748dae2256672ac699b927372
Alexander Pustovalov <Pustovalov.al.an@gmail.com>
2024-04-07 13:19:53 +0300

    FIX VERSION (OLD)

--- src/app/App.js
+++ src/app/App.js
@@ -6,13 +6,9 @@ import homeIcon from "../shared/home.png"
 import heartIcon from "../shared/heart.png"
 import pushIcon from "../shared/push.png"
 import cartIcon from "../shared/cart.png"
-<<<<<<< HEAD
 import Cart from '../entities/Cart/Cart';
-=======
-import MyCart from '../pages/MyCart/MyCart';
 import CurrentItem from '../pages/currentItem/CurrentItem';
 import { Link, Route, Routes } from 'react-router-dom';
->>>>>>> 1b742343f48b71c2d5cbbcb91670ca1719e96096
 
 function App() {
   return (
@@ -21,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Catalog/>} />
         <Route path="/item" element={<CurrentItem/>}/>
-        <Route path='/cart' element={<MyCart/>}/>
+        <Route path='/cart' element={<Cart/>}/>
       </Routes>
         <div className={styles.navbar}>
           <img alt='' src={pushIcon} />
@@ -30,7 +26,7 @@ function App() {
           <Link to={"/cart"}><img alt='' src={cartIcon} /></Link>
           <img alt='' src={heartIcon} />
         </div>
-        <Cart />
+        {/* <Cart /> */}
     </div>
   )
 }
--- src/shared/ImportImages.jsx
+++ src/shared/ImportImages.jsx
@@ -8,25 +8,15 @@ import photo2 from "./photo2.png"
 import push from "./push.png"
 import searchImage from "./searchImage.svg"
 import user from "./user.png"
-<<<<<<< HEAD
 import plus from "./plus.svg"
 import minus from "./minus.svg"
-
-const Images = {
-    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, plus, minus
-=======
 import exitImage from "./exit.svg"
 import exampleImage from "./example.png"
 import shareImage from "./share.svg"
 import userIcon from "./userIcon.svg"
 
 const Images = {
-<<<<<<< HEAD
-    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, exampleImage, exitImage, shareImage, userIcon
-=======
-    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, exampleImage, exitImage, shareImage
->>>>>>> 1b742343f48b71c2d5cbbcb91670ca1719e96096
->>>>>>> e1d556f6c32c1a9d77690938ae9c2fae5bd5d14d
+    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, exampleImage, exitImage, shareImage, userIcon, minus, plus
 }
 
 
