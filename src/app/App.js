0b8df7ba72f5b8590ffd561af21dd78fb589742d
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-04-07 15:20:31 +0300

    .

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Catalog from '../pages/catalog/Catalog';
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import styles from "./app.module.css"
 import userIcon from "../shared/user.png"
 import homeIcon from "../shared/home.png"
 import heartIcon from "../shared/heart.png"
 import pushIcon from "../shared/push.png"
 import cartIcon from "../shared/cart.png"
-<<<<<<< HEAD
-import Cart from '../entities/Cart/Cart';
-=======
 import MyCart from '../pages/MyCart/MyCart';
 import CurrentItem from '../pages/currentItem/CurrentItem';
-import { Link, Route, Routes } from 'react-router-dom';
->>>>>>> 1b742343f48b71c2d5cbbcb91670ca1719e96096
-
+import { Link } from "react-router-dom"
 function App() {
   return (
 
@@ -30,7 +26,7 @@ function App() {
           <Link to={"/cart"}><img alt='' src={cartIcon} /></Link>
           <img alt='' src={heartIcon} />
         </div>
-        <Cart />
+        <MyCart />
     </div>
   )
 }
--- src/shared/ImportImages.jsx
+++ src/shared/ImportImages.jsx
@@ -8,21 +8,11 @@ import photo2 from "./photo2.png"
 import push from "./push.png"
 import searchImage from "./searchImage.svg"
 import user from "./user.png"
-<<<<<<< HEAD
 import plus from "./plus.svg"
 import minus from "./minus.svg"
-
+import vector from "./Vector.svg"
 const Images = {
-    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, plus, minus
-=======
-import exitImage from "./exit.svg"
-import exampleImage from "./example.png"
-import shareImage from "./share.svg"
-
-
-const Images = {
-    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, exampleImage, exitImage, shareImage
->>>>>>> 1b742343f48b71c2d5cbbcb91670ca1719e96096
+    addToCart, cart, heart, home, like, photo1, photo2, push, searchImage, user, plus, minus, vector
 }
 
 
